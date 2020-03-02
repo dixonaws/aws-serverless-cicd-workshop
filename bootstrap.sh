7f41934af6711161c18c78f0b329e6ba53f0b478
John Dixon <dixonaws@amazon.com>
2020-03-02 10:16:30 +0000

    Updated bootstrap.sh script to install JDK

--- bootstrap.sh
+++ bootstrap.sh
@@ -96,6 +96,7 @@ function install_maven() {
     echo "export M2_HOME=/usr/local/apache-maven" >> ~/.profile
     echo "export M2=/usr/local/apache-maven/bin" >> ~/.profile
     echo "export PATH=/usr/local/apache-maven/bin:$PATH" >> ~/.profile
+    echo "export PATH=/usr/local/apache-maven/bin:$PATH" >> ~/.bashrc
     
 }
 
@@ -110,7 +111,6 @@ function main() {
     echo -e "${RED} [!!!!!!!!!] Open up a new terminal to reflect changes ${NC}"
     _logger "[+] Restarting Shell to reflect changes"
     exec ${SHELL}
-    source ~/.profile
 }
 
 main
