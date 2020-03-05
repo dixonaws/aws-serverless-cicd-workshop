1105513be7e6a6b9b143038a557e4122ef463ec4
dixonaws <[]May10!@#>
2020-03-05 15:43:51 +0100

    Cleanup of instructions

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git add .
+git commit -m "Cleanup of instructions"
+git push
+hugo
+cd public
+aws s3 sync . s3://www.dixonaws.com/aws-serverless-cdci-workshop/
+cd ..
+aws cloudfront
+aws cloudfront create-invalidation --distribution-id "E1ZEGQOWEQTU7I" --paths "/aws-serverless-cdci-workshop/*"
